6181a07da493a0a623cdd2998167cdd00c3046d5
Furkan Albayrak <furkanalb42@gmail.com>
2025-08-30 17:27:22 +0300

    RouterConfig yeni eklemeler yapildi

--- user-department-management-system/frontent/src/config/RouterConfig.jsx
+++ user-department-management-system/frontent/src/config/RouterConfig.jsx
@@ -3,15 +3,44 @@ import {Route, Routes} from 'react-router-dom'
 import AllUsers from '../filterComponents/AllUsers'
 import NameUsers from '../filterComponents/NameUsers'
 import IdUsers from '../filterComponents/IdUsers'
+import Hero from '../components/Hero'
+import AddUser from '../panelComponents/AddUser'
+import UpdateUser from '../panelComponents/UpdateUser'
 
 function RouterConfig() {
   return (
     <div>
         <Routes>
-          <Route path='/' element={<AllUsers/>} />
-          <Route path='/all-users' element={<AllUsers/>} />
-          <Route path='/name' element={<NameUsers/>} />
-          <Route path='/ID' element={<IdUsers/>} />
+          <Route path='/' element={
+            <>
+              <Hero/>
+              <AllUsers/>
+            </>
+          }/>
+          <Route path='/users/all-users' element={
+            <>
+              <Hero/>
+              <AllUsers/>  
+            </>
+          } />
+          <Route path='/users/name' element={
+            <>
+              <Hero/>
+              <NameUsers/>
+            </>
+          } />
+          <Route path='/users/ID' element={
+            <>
+              <Hero/>
+              <IdUsers/>
+            </>
+          } />
+          <Route path='/users/add-user' element={<AddUser/>} >
+
+          </Route>
+          <Route path='/users/update-user' element={<UpdateUser/>} > 
+
+          </Route>
         </Routes>
     </div>
   )
