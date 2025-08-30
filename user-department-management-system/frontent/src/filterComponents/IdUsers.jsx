6da502a5b55d093d393e2b37d5559e35da3b3dea
Furkan Albayrak <furkanalb42@gmail.com>
2025-08-30 17:26:51 +0300

    filter componentler input kontrolleri yapildi

--- user-department-management-system/frontent/src/filterComponents/IdUsers.jsx
+++ user-department-management-system/frontent/src/filterComponents/IdUsers.jsx
@@ -9,8 +9,14 @@ function IdUsers() {
 
     const filterData = async () => {
         try {
-            const response = await axios.get(`http://localhost:3000/users/id/${getId}`);
-            setData(response.data);
+            if (getId) {
+                const response = await axios.get(`http://localhost:3000/users/id/${getId}`);
+                setData(response.data);
+            }
+            else{
+                setData([]);
+            }
+
         } catch (error) {
             console.error(error);
         }
@@ -18,42 +24,48 @@ function IdUsers() {
 
     return (
 
-        <div>
+        <div className='pb-16'>
             <div className='flex items-center -space-x-14 ml-8'>
-                <input type="text" value={getId} onChange={(e) => setGetId(e.target.value)} className='w-10/12 text-xl bg-red-100 pl-10 pr-20 py-3 rounded-full outline-none' placeholder="Aramak istediğiniz ID'yi giriniz"/>
+                <input type="text" value={getId} onChange={(e) => setGetId(e.target.value)} className='w-1/2 text-xl bg-gray-200 pl-10 pr-20 py-3 rounded-full outline-none' placeholder="Aramak istediğiniz ID'yi giriniz" />
                 <div className='z-10 text-3xl cursor-pointer' onClick={filterData}>
                     <IoIosSearch />
                 </div>
             </div>
 
-             <table className='w-full table-auto border-collapse text-lg ml-10 mt-10'>
-                <thead>
-                    <tr>
-                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">ID</th>
-                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Ad</th>
-                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Soyad</th>
-                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Email</th>
-                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman No</th>
-                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        data.map((element, index) => (
-                            <tr key={index}>
-                                <td className="border border-gray-300 px-3 py-2"> {element.id} </td>
-                                <td className="border border-gray-300 px-3 py-2"> {element.name} </td>
-                                <td className="border border-gray-300 px-3 py-2"> {element.surname} </td>
-                                <td className="border border-gray-300 px-3 py-2"> {element.email} </td>
-                                <td className="border border-gray-300 px-3 py-2"> {element.department_id} </td>
-                                <td className="border border-gray-300 px-3 py-2"> {element.department_name} </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+            {
+                (data.length === 0) ? (
+                    <div className='p-4 text-xl text-red-500 ml-6'>Lütfen Data basede bulunan bir ID giriniz</div>
+                )
+                    : (
+                        <table className='w-10/12 table-auto border-collapse text-lg ml-10 mt-10'>
+                            <thead>
+                                <tr>
+                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">ID</th>
+                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Ad</th>
+                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Soyad</th>
+                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Email</th>
+                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman No</th>
+                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map((element, index) => (
+                                        <tr key={index}>
+                                            <td className="border border-gray-300 px-3 py-2"> {element.id} </td>
+                                            <td className="border border-gray-300 px-3 py-2"> {element.name} </td>
+                                            <td className="border border-gray-300 px-3 py-2"> {element.surname} </td>
+                                            <td className="border border-gray-300 px-3 py-2"> {element.email} </td>
+                                            <td className="border border-gray-300 px-3 py-2"> {element.department_id} </td>
+                                            <td className="border border-gray-300 px-3 py-2"> {element.department_name} </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    )
+            }
         </div>
-
     )
 }
 
--- user-department-management-system/frontent/src/filterComponents/NameUsers.jsx
+++ user-department-management-system/frontent/src/filterComponents/NameUsers.jsx
@@ -9,17 +9,23 @@ function NameUsers() {
 
     const filterData = async () => {
         try {
-            const response = await axios.get(`http://localhost:3000/users/name/${inputValue}`);
-            setData(response.data);
+            if (inputValue) {
+                const response = await axios.get(`http://localhost:3000/users/name/${inputValue}`);
+                setData(response.data);
+            }
+            else {
+                setData([]);
+            }
+
         } catch (error) {
             console.error(error);
         }
     }
 
     return (
-        <div>
+        <div className='pb-16'>
             <div className='flex items-center -space-x-14 ml-8'>
-                <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='w-10/12 text-xl bg-red-100 pl-10 pr-20 py-3 rounded-full outline-none' placeholder='Aramak istediğiniz ismi giriniz' />
+                <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='w-1/2 text-xl bg-gray-200 pl-10 pr-20 py-3 rounded-full outline-none' placeholder='Aramak istediğiniz ismi giriniz' />
                 <div className='z-10 text-3xl cursor-pointer' onClick={filterData}>
                     <IoIosSearch />
                 </div>
@@ -27,10 +33,10 @@ function NameUsers() {
 
             {
                 (data.length === 0) ? (
-                    <div>Lütfen Data basede bulunan bir ad giriniz</div>
+                    <div className='p-4 text-xl text-red-500 ml-6'>Lütfen Data basede bulunan bir ad giriniz</div>
                 )
                     : (
-                        <table className='w-full table-auto border-collapse text-lg ml-10 mt-10'>
+                        <table className='w-10/12 table-auto border-collapse text-lg ml-10 mt-10'>
                             <thead>
                                 <tr>
                                     <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">ID</th>
@@ -58,8 +64,6 @@ function NameUsers() {
                         </table>
                     )
             }
-
-
         </div>
     )
 }
