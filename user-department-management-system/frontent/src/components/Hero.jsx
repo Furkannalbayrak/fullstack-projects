b8e2da1b8c785533c30a094342c9a78907ba6a3f
Furkan Albayrak <furkanalb42@gmail.com>
2025-08-30 17:21:33 +0300

    navigate yonlendirmeleri duzenlendi

--- user-department-management-system/frontent/src/components/Hero.jsx
+++ user-department-management-system/frontent/src/components/Hero.jsx
@@ -7,9 +7,9 @@ function Hero() {
     return (
         <div className='w-full'>
             <div className='flex gap-14 p-10 mx-auto'>
-                <div onClick={()=> navigate("/all-users")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>Tüm Kullanıcıları Listele</div>
-                <div onClick={()=> navigate("/name")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>İsme Göre Listele</div>
-                <div onClick={()=> navigate("/ID")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>ID'ye Göre Listele</div>
+                <div onClick={()=> navigate("/users/all-users")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>Tüm Kullanıcıları Listele</div>
+                <div onClick={()=> navigate("/users/name")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>İsme Göre Listele</div>
+                <div onClick={()=> navigate("/users/ID")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>ID'ye Göre Listele</div>
             </div>
         </div>
     )
