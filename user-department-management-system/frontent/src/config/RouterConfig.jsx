705bd85db3ce39cedf513d0b51f2074e51523f8c
Furkan Albayrak <furkanalb42@gmail.com>
2025-09-01 16:19:08 +0300

    RouterConfig componentler eklendi

--- user-department-management-system/frontent/src/config/RouterConfig.jsx
+++ user-department-management-system/frontent/src/config/RouterConfig.jsx
@@ -4,8 +4,13 @@ import AllUsers from '../filterComponents/AllUsers'
 import NameUsers from '../filterComponents/NameUsers'
 import IdUsers from '../filterComponents/IdUsers'
 import Hero from '../components/Hero'
-import AddUser from '../panelComponents/AddUser'
-import UpdateUser from '../panelComponents/UpdateUser'
+import AddUser from '../userPanelComponents/AddUser'
+import UpdateUser from '../userPanelComponents/UpdateUser'
+import DeleteUser from '../userPanelComponents/DeleteUser'
+import ListDepartment from '../departmentPanelComponents/ListDepartment'
+import AddDepartment from '../departmentPanelComponents/AddDepartment'
+import UpdateDepartment from '../departmentPanelComponents/UpdateDepartment'
+import DeleteDepartment from '../departmentPanelComponents/DeleteDepartment'
 
 function RouterConfig() {
   return (
@@ -39,6 +44,21 @@ function RouterConfig() {
 
           </Route>
           <Route path='/users/update-user' element={<UpdateUser/>} > 
+
+          </Route>
+          <Route path='/users/delete-user' element={<DeleteUser/>}>
+
+          </Route>
+          <Route path='/departments/all-departments' element={<ListDepartment/>}>
+
+          </Route>
+          <Route path='/departments/add-department' element={<AddDepartment/>}>
+
+          </Route>
+          <Route path='/departments/update-department' element={<UpdateDepartment/>}>
+
+          </Route>
+          <Route path='/departments/delete-department' element={<DeleteDepartment/>}>
 
           </Route>
         </Routes>
