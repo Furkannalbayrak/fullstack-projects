0ff4ef9f6471613dc40655abc979e93068f5a6c6
Furkan Albayrak <furkanalb42@gmail.com>
2025-09-01 16:22:31 +0300

    Departman listesi componenti eklendi

---
+++ user-department-management-system/frontent/src/departmentPanelComponents/ListDepartment.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function ListDepartment() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(()=>{
+        const getData = async ()=>{
+            try {
+                const response = await axios.get("http://localhost:3000/departments");
+                setData(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getData();
+    },[])
+
+    return (
+        <div className='py-16'>
+            <table className='w-1/2 table-auto border-collapse text-lg ml-16'>
+                <thead>
+                    <tr>
+                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman No</th>
+                        <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((element, index) => (
+                            <tr key={index}>
+                                <td className="border border-gray-300 px-3 py-2"> {element.id} </td>
+                                <td className="border border-gray-300 px-3 py-2"> {element.department_name} </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ListDepartment
