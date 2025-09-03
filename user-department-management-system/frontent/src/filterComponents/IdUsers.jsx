7b2aa4ba46b4106e5b6a838f92095928e7fa0035
Furkan Albayrak <furkanalb42@gmail.com>
2025-09-03 16:06:09 +0300

    filterComponents responsive eklendi

--- user-department-management-system/frontent/src/filterComponents/IdUsers.jsx
+++ user-department-management-system/frontent/src/filterComponents/IdUsers.jsx
@@ -25,8 +25,8 @@ function IdUsers() {
     return (
 
         <div className='pb-16'>
-            <div className='flex items-center -space-x-14 ml-8'>
-                <input type="text" value={getId} onChange={(e) => setGetId(e.target.value)} className='w-1/2 text-xl bg-gray-200 pl-10 pr-20 py-3 rounded-full outline-none' placeholder="Aramak istediğiniz ID'yi giriniz" />
+            <div className='flex items-center -space-x-14 w-10/12 mx-auto'>
+                <input type="text" value={getId} onChange={(e) => setGetId(e.target.value)} className='w-96 lg:text-xl md:text-lg text-md bg-gray-200 pl-10 pr-20 py-3 rounded-full outline-none' placeholder="Aramak istediğiniz ID'yi giriniz" />
                 <div className='z-10 text-3xl cursor-pointer' onClick={filterData}>
                     <IoIosSearch />
                 </div>
@@ -34,30 +34,30 @@ function IdUsers() {
 
             {
                 (data.length === 0) ? (
-                    <div className='p-4 text-xl text-red-500 ml-6'>Lütfen Data basede bulunan bir ID giriniz</div>
+                    <div className='p-4 lg:text-xl md:text-lg text-md text-red-500 w-10/12 mx-auto'>Lütfen Data basede bulunan bir ID giriniz</div>
                 )
                     : (
-                        <table className='w-10/12 table-auto border-collapse text-lg ml-10 mt-10'>
+                        <table className='w-10/12 mx-auto table-auto border-collapse lg:text-lg md:text-sm text-xs mt-10'>
                             <thead>
                                 <tr>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">ID</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Ad</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Soyad</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Email</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman No</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">ID</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Ad</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Soyad</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Email</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Departman No</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Departman</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {
                                     data.map((element, index) => (
                                         <tr key={index}>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.id} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.name} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.surname} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.email} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.department_id} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.department_name} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.id} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.name} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.surname} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.email} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.department_id} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.department_name} </td>
                                         </tr>
                                     ))
                                 }
--- user-department-management-system/frontent/src/filterComponents/NameUsers.jsx
+++ user-department-management-system/frontent/src/filterComponents/NameUsers.jsx
@@ -24,8 +24,8 @@ function NameUsers() {
 
     return (
         <div className='pb-16'>
-            <div className='flex items-center -space-x-14 ml-8'>
-                <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='w-1/2 text-xl bg-gray-200 pl-10 pr-20 py-3 rounded-full outline-none' placeholder='Aramak istediğiniz ismi giriniz' />
+            <div className='flex items-center -space-x-14 w-10/12 mx-auto'>
+                <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='w-96 lg:text-xl md:text-lg text-md bg-gray-200 pl-10 pr-20 py-3 rounded-full outline-none' placeholder='Aramak istediğiniz ismi giriniz' />
                 <div className='z-10 text-3xl cursor-pointer' onClick={filterData}>
                     <IoIosSearch />
                 </div>
@@ -33,30 +33,30 @@ function NameUsers() {
 
             {
                 (data.length === 0) ? (
-                    <div className='p-4 text-xl text-red-500 ml-6'>Lütfen Data basede bulunan bir ad giriniz</div>
+                    <div className='p-4 lg:text-xl md:text-lg text-md text-red-500 w-10/12 mx-auto'>Lütfen Data basede bulunan bir ad giriniz</div>
                 )
                     : (
-                        <table className='w-10/12 table-auto border-collapse text-lg ml-10 mt-10'>
+                        <table className='w-10/12 mx-auto table-auto border-collapse lg:text-lg md:text-sm text-xs mt-10'>
                             <thead>
                                 <tr>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">ID</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Ad</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Soyad</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Email</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman No</th>
-                                    <th className="border border-gray-400 px-4 py-2 text-left bg-blue-100">Departman</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">ID</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Ad</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Soyad</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Email</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Departman No</th>
+                                    <th className="border border-gray-400 lg:px-4 px-2 py-2 text-left bg-blue-100">Departman</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {
                                     data.map((element, index) => (
                                         <tr key={index}>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.id} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.name} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.surname} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.email} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.department_id} </td>
-                                            <td className="border border-gray-300 px-3 py-2"> {element.department_name} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.id} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.name} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.surname} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.email} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.department_id} </td>
+                                            <td className="border border-gray-300 lg:px-3 px-1 py-2"> {element.department_name} </td>
                                         </tr>
                                     ))
                                 }
