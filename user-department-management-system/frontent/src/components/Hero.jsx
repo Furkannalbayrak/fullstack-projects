4dbd2ab771052a749e8d186e4d88d4af5a7eb658
Furkan Albayrak <furkanalb42@gmail.com>
2025-09-03 16:04:21 +0300

    Hero responsive eklendi

--- user-department-management-system/frontent/src/components/Hero.jsx
+++ user-department-management-system/frontent/src/components/Hero.jsx
@@ -5,8 +5,8 @@ function Hero() {
     const navigate = useNavigate();
 
     return (
-        <div className='w-full'>
-            <div className='flex gap-14 p-10 mx-auto'>
+        <div className='w-full '>
+            <div className='flex flex-col lg:flex-row lg:gap-14 gap-2 p-10 mx-auto w-11/12'>
                 <div onClick={()=> navigate("/users/all-users")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>Tüm Kullanıcıları Listele</div>
                 <div onClick={()=> navigate("/users/name")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>İsme Göre Listele</div>
                 <div onClick={()=> navigate("/users/ID")} className='bg-gray-200 px-5 py-3 hover:bg-gray-300 cursor-pointer transition duration-200 rounded-xl'>ID'ye Göre Listele</div>
