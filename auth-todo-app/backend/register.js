7fe813d58d28fa4decd196af7784c81454f8abde
Furkan Albayrak <furkanalb42@gmail.com>
2025-09-20 18:42:54 +0300

    backend register islemleri tamamlandi

---
+++ auth-todo-app/backend/register.js
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+import pool from './database.js'
+import bcrypt from 'bcrypt'
+
+const router = Router();
+
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const result = await pool.query(
+            `INSERT INTO users(name, email, password_hash)
+            values($1, $2, $3)
+            RETURNING *`,
+            [name, email, hashedPassword]
+        );
+        res.status(201).send("Kayıt başarıyla gerçekleşti");
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send("Bu email zaten kayıtlı");
+    }
+});
+
+export default router;
