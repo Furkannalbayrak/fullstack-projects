5f61e6f4bac143342a61229403f6fc74b251109b
Furkan Albayrak <furkanalb42@gmail.com>
2025-10-03 21:32:07 +0300

    addTodo ve deleteTodo fonksiyonlari eklendi

--- auth-todo-app/frontend/src/components/HomePage.jsx
+++ auth-todo-app/frontend/src/components/HomePage.jsx
@@ -1,18 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { AiOutlineFileAdd } from "react-icons/ai";
-import { MdCheckBoxOutlineBlank } from "react-icons/md";
 import { FaRegSquare } from "react-icons/fa";
-
 import { IoCheckbox } from "react-icons/io5";
 import { HiTrash } from "react-icons/hi";
 
 function HomePage() {
-
   const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
 
   useEffect(() => {
-
     const fetchTodos = async () => {
       try {
         const token = localStorage.getItem("token");
@@ -27,11 +24,48 @@ function HomePage() {
         console.error(error.message);
       }
     }
-
     fetchTodos();
-
-    console.log(todos);
   }, []);
+
+  const addTodo = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+
+    if (text) {
+      try {
+        const response = await axios.post("http://localhost:3000/api/todos", { text }, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+        
+        setTodos((prevTodos) => [...prevTodos, response.data]);
+        setText("");
+        console.log("Todo başarıyla eklendi");
+
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    else {
+      alert("Görev eklemek için birşeyler yazın");
+    }
+  }
+
+  const deleteTodo = async (todoID) => {
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await axios.delete(`http://localhost:3000/api/todos/${todoID}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      setTodos((prevTodo) => prevTodo.filter((todo) => todo.id !== todoID));
+      console.log("Todo başarıyla silindi");
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
 
   return (
     <div className='w-screen'>
@@ -49,14 +83,20 @@ function HomePage() {
               <p className='text-center text-lg mt-2'>Görevlerinizi yönetmeye başlayın</p>
 
               <div className='text-center mt-5'>
-                <form className='flex gap-3 bg-white rounded-xl shadow-[0_5px_6px_-1px_rgba(0,0,0,0.1)] p-6'>
-                  <input type="text" placeholder='Yeni görev ekle...' className='px-4 py-3 outline-none border-2 border-gray-300 rounded-lg w-full text-lg' />
-                  <button className='bg-blue-400 text-white px-7 rounded-md text-lg font-semibold transition duration-200 hover:bg-blue-600'>Ekle</button>
+                <form onSubmit={addTodo} className='flex gap-3 bg-white rounded-xl shadow-[0_5px_6px_-1px_rgba(0,0,0,0.1)] p-6'>
+                  <input
+                    type="text"
+                    placeholder='Yeni görev ekle...'
+                    className='px-4 py-3 outline-none border-2 border-gray-300 rounded-lg w-full text-lg'
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                  />
+                  <button type='submit' className='bg-blue-400 text-white px-7 rounded-md text-lg font-semibold transition duration-200 hover:bg-blue-600'>Ekle</button>
                 </form>
               </div>
             </div>
 
-            <div className='bg-white rounded-xl mt-8 shadow-[0_5px_6px_-1px_rgba(0,0,0,0.1)]'>
+            <div className='bg-white rounded-xl mt-8 shadow-[0_5px_6px_-1px_rgba(0,0,0,0.1)] overflow-y-auto max-h-[380px]'>
               {todos.length === 0 ?
                 (
                   <div className='flex flex-col gap-4 text-center items-center p-8'>
@@ -71,20 +111,22 @@ function HomePage() {
                 ) : (
                   <div>
                     {
-                      todos.map((element, index) => (
+                      todos.map((element) => (
                         <div className='flex items-center justify-between p-4 border-2 border-gray-100' key={element.id}>
                           <div className='flex items-center text-[27px] text-gray-600 cursor-pointer'>
                             {
                               element.is_done === "false" ? (
                                 <IoCheckbox className='' />
-
                               ) : (
                                 <FaRegSquare />
                               )
                             }
                             <p className='text-lg pl-3 pr-8 cursor-default'>{element.text}</p>
                           </div>
-                          <p className='text-red-500 text-2xl transition duration-100 hover:text-red-700 cursor-pointer'><HiTrash /></p>
+                          <p
+                            onClick={() => deleteTodo(element.id)}
+                            className='text-red-500 text-2xl transition duration-100 hover:text-red-700 cursor-pointer'><HiTrash />
+                          </p>
                         </div>
                       ))
                     }
