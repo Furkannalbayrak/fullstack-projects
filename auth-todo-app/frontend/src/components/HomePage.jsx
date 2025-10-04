e6082f3aa641f2a48ba3905756a8b02b8b2a0882
Furkan Albayrak <furkanalb42@gmail.com>
2025-10-04 21:29:06 +0300

    HomePage.jsx logout butonu eklendi

--- auth-todo-app/frontend/src/components/HomePage.jsx
+++ auth-todo-app/frontend/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { AiOutlineFileAdd } from "react-icons/ai";
 import { FaRegSquare } from "react-icons/fa";
 import { IoCheckbox } from "react-icons/io5";
@@ -8,6 +9,8 @@ import { HiTrash } from "react-icons/hi";
 function HomePage() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
+  const [logout, setLogout] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -36,7 +39,7 @@ function HomePage() {
         const response = await axios.post("http://localhost:3000/api/todos", { text }, {
           headers: { Authorization: `Bearer ${token}` }
         });
-        
+
         setTodos((prevTodos) => [...prevTodos, response.data]);
         setText("");
         console.log("Todo başarıyla eklendi");
@@ -66,18 +69,76 @@ function HomePage() {
     }
   }
 
+  const updateIsDone = async (todoID, is_done) => {
+    const token = localStorage.getItem("token");
+    is_done = !is_done;
+
+    try {
+      const response = await axios.put(`http://localhost:3000/api/todos/${todoID}`, { is_done }, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === todoID ? { ...todo, is_done: response.data.is_done } : todo
+        )
+      )
+      console.log("Todo başarıyla güncellendi");
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
+  const showLogout = () => {
+    setLogout(true);
+  }
+  const closeLogout = () => {
+    setLogout(false);
+  }
 
   return (
     <div className='w-screen'>
-      <div className='px-10'>
+      <div className='sm:px-10'>
         <div className='h-screen max-w-2xl mx-auto overflow-hidden'>
 
           <div className='flex justify-between bg-blue-800 p-5'>
             <h2 className='text-3xl font-bold text-white'>Todo App</h2>
-            <button className='text-white bg-red-600 px-4 py-2 rounded-md transition duration-150 hover:bg-red-700'>Çıkış Yap</button>
+            <button
+              onClick={showLogout}
+              className='text-white bg-red-600 px-4 py-2 rounded-md transition duration-150 hover:bg-red-700'>Çıkış Yap</button>
           </div>
 
           <div className='p-6 bg-gray-100 h-full'>
+
+            {/* Modal */}
+            {logout && (
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center z-50">
+                <div className="bg-white p-6 rounded-md shadow-lg max-w-xs w-full">
+                  <p className="text-lg text-center">Çıkış yapmak istediğinizden</p>
+                  <p className="text-lg text-center">emin misiniz?</p>
+                  <div className="flex justify-center gap-8 mt-4">
+                    <button
+                      onClick={handleLogout}
+                      className="bg-red-600 transition duration-150 hover:bg-red-700 text-white px-5 py-2 rounded-md"
+                    >
+                      Evet
+                    </button>
+                    <button
+                      onClick={closeLogout}
+                      className="bg-gray-500 transition duration-150 hover:bg-gray-600 text-white px-5 py-2 rounded-md"
+                    >
+                      Hayır
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+
             <div>
               <p className='text-3xl font-semibold text-center'>Hoş Geldin</p>
               <p className='text-center text-lg mt-2'>Görevlerinizi yönetmeye başlayın</p>
@@ -91,12 +152,12 @@ function HomePage() {
                     value={text}
                     onChange={(e) => setText(e.target.value)}
                   />
-                  <button type='submit' className='bg-blue-400 text-white px-7 rounded-md text-lg font-semibold transition duration-200 hover:bg-blue-600'>Ekle</button>
+                  <button type='submit' className='bg-blue-400 text-white sm:px-7 px-4 rounded-md text-lg font-semibold transition duration-200 hover:bg-blue-600'>Ekle</button>
                 </form>
               </div>
             </div>
 
-            <div className='bg-white rounded-xl mt-8 shadow-[0_5px_6px_-1px_rgba(0,0,0,0.1)] overflow-y-auto max-h-[380px]'>
+            <div className='bg-white rounded-xl mt-6 shadow-[0_5px_6px_-1px_rgba(0,0,0,0.1)] overflow-y-auto sm:max-h-[380px] max-h-[320px]'>
               {todos.length === 0 ?
                 (
                   <div className='flex flex-col gap-4 text-center items-center p-8'>
@@ -115,13 +176,20 @@ function HomePage() {
                         <div className='flex items-center justify-between p-4 border-2 border-gray-100' key={element.id}>
                           <div className='flex items-center text-[27px] text-gray-600 cursor-pointer'>
                             {
-                              element.is_done === "false" ? (
-                                <IoCheckbox className='' />
+                              element.is_done === false ? (
+                                <FaRegSquare onClick={() => updateIsDone(element.id, element.is_done)} />
                               ) : (
-                                <FaRegSquare />
+                                <IoCheckbox onClick={() => updateIsDone(element.id, element.is_done)} className='text-blue-500 rounded-md' />
                               )
                             }
-                            <p className='text-lg pl-3 pr-8 cursor-default'>{element.text}</p>
+
+                            {
+                              element.is_done === false ? (
+                                <p className='text-lg pl-3 pr-3 cursor-default'>{element.text}</p>
+                              ) : (
+                                <p className='text-lg pl-3 pr-3 cursor-default text-gray-400 line-through'>{element.text}</p>
+                              )
+                            }
                           </div>
                           <p
                             onClick={() => deleteTodo(element.id)}
